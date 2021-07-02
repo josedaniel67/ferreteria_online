93d303cb6ee0f08fc289dfef118f86f06cc2ca28
Edward Brito <Edwardbrito11@hotmail.com>
2021-07-02 12:27:02 -0400

    update

---
+++ nosotros.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Ferreteria JM</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/nosotros.css" rel="stylesheet" />
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="./fontawesome/css/all.css">
+
+    </head>
+
+</head>
+
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#!"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-0 mb-lg-0 ms-lg-4">
+
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="./plantilla.php">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./nosotros.php">Acerca </a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Articulos Comprados</a></li>
+
+                </ul>
+                <form class="d-flex">
+                    <button class="btn btn-outline-dark" type="submit">
+                        <i class="bi-cart-fill me-1"></i>
+                        Carro
+                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                    </button>
+                    <a href="./loguin.php">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">
+                            <i class="fas fa-sign-in-alt"></i>
+
+                            Iniciar sesión
+                        </button>
+                    </a>
+
+            </div>
+        </div>
+    </nav>
+
+
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 ">
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <p><strong>Nosotros</strong></p>
+                                <span>Nuestros productos comercializados: Tornillería, tuercas, wasas, arandelas, clavos, grapas, espárragos, remaches, chazos, abrazaderas, acoples, brocas, cajas de herramientas, calibradores, candados y cerraduras, cintas, abrasivos, herramientas manuales y eléctricas, entre otros artículos de ferretería en general.</span>
+
+                                $10.00
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <p><strong>Nosotros</strong></p>
+                                <span>Nuestros productos comercializados: Tornillería, tuercas, wasas, arandelas, clavos, grapas, espárragos, remaches, chazos, abrazaderas, acoples, brocas, cajas de herramientas, calibradores, candados y cerraduras, cintas, abrasivos, herramientas manuales y eléctricas, entre otros artículos de ferretería en general.</span>
+
+                                $10.00
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <p><strong>Nosotros</strong></p>
+                                <span>Nuestros productos comercializados: Tornillería, tuercas, wasas, arandelas, clavos, grapas, espárragos, remaches, chazos, abrazaderas, acoples, brocas, cajas de herramientas, calibradores, candados y cerraduras, cintas, abrasivos, herramientas manuales y eléctricas, entre otros artículos de ferretería en general.</span>
+
+                                $10.00
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="container px-4 px-lg-5 mt-5">
+            <p><strong>Vision</strong></p>
+            <span>Nuestros productos comercializados: Tornillería, tuercas, wasas, arandelas, clavos, grapas, espárragos, remaches, chazos, abrazaderas, acoples, brocas, cajas de herramientas, calibradores, candados y cerraduras, cintas, abrasivos, herramientas manuales y eléctricas, entre otros artículos de ferretería en general.</span>
+        </div> -->
+
+
+    </section>
+</body>
+
+</html>
