1c4a39b1db58ff11c7295b1bbfbea813f3db6032
Edward Brito <Edwardbrito11@hotmail.com>
2021-07-02 11:50:57 -0400

    update

--- loguin.php
+++ loguin.php
@@ -5,6 +5,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,51 +15,39 @@
     <link rel="stylesheet" href="css/loguin.css">
     <title>/Registrar</title>
 </head>
+
 <body>
     <div id="logreg-forms">
         <form class="form-signin">
-            <h1 class="h3 mb-3 font-weight-normal" style="text-align: center"> REGISTRARSE</h1>
-            <div class="social-login">
-                <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Iniciar con Facebook</span> </button>
-                <button class="btn google-btn social-btn" type="button"><span><i class="fab fa-google-plus-g"></i> Iniciar con Google+</span> </button>
-            </div>
-            <p style="text-align:center"> o </p>
-            <input type="email" id="inputEmail" class="form-control" placeholder="correo electronico" required="" autofocus="">
-            <input type="password" id="inputPassword" class="form-control" placeholder="contraseña" required="">
-            
+            <h1 class="h3 mb-3 font-weight-normal" style="text-align: center"> INICIAR SESIÓN</h1>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required="" autofocus="">
+            <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required="">
+
             <button class="btn btn-success btn-block" type="submit"><i class="fas fa-sign-in-alt"></i> Registrarse</button>
             <a href="#" id="forgot_pswd">Olvidaste la Contraseña?</a>
             <hr>
             <!-- <p>Don't have an account!</p>  -->
             <button class="btn btn-primary btn-block" type="button" id="btn-signup"><i class="fas fa-user-plus"></i> Crear una Nueva Cuenta</button>
-            </form>
+        </form>
 
-            <form action="/reset/password/" class="form-reset">
-                <input type="email" id="resetEmail" class="form-control" placeholder="correo electronico" required="" autofocus="">
-                <button class="btn btn-primary btn-block" type="submit">Repetir Contraseña</button>
-                <a href="#" id="cancel_reset"><i class="fas fa-angle-left"></i> Atras</a>
-            </form>
-            
-            <form action="/signup/" class="form-signup">
-                <div class="social-login">
-                    <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Iniciar con Facebook </span> </button>
-                </div>
-                <div class="social-login">
-                    <button class="btn google-btn social-btn" type="button"><span><i class="fab fa-google-plus-g"></i> Iniciar con Google+</span> </button>
-                </div>
-                
-                <p style="text-align:center">O</p>
+        <form action="/reset/password/" class="form-reset">
+            <input type="email" id="resetEmail" class="form-control" placeholder="correo electronico" required="" autofocus="">
+            <button class="btn btn-primary btn-block" type="submit">Repetir Contraseña</button>
+            <a href="#" id="cancel_reset"><i class="fas fa-angle-left"></i> Atras</a>
+        </form>
 
-                <input type="text" id="user-name" class="form-control" placeholder="nombre completo" required="" autofocus="">
-                <input type="email" id="user-email" class="form-control" placeholder="correo electronico" required autofocus="">
-                <input type="password" id="user-pass" class="form-control" placeholder="contraseña" required autofocus="">
-                <input type="password" id="user-repeatpass" class="form-control" placeholder="Repetir contraseña" required autofocus="">
+        <form action="/signup/" class="form-signup">
+            <h1 class="h3 mb-3 font-weight-normal" style="text-align: center"> REGISTRARSE</h1>
+            <input type="text" id="user-name" class="form-control" placeholder="Nombre completo" required="" autofocus="">
+            <input type="email" id="user-email" class="form-control" placeholder="Correo electrónico" required autofocus="">
+            <input type="password" id="user-pass" class="form-control" placeholder="Contraseña" required autofocus="">
+            <input type="password" id="user-repeatpass" class="form-control" placeholder="Repetir contraseña" required autofocus="">
 
-                <button class="btn btn-primary btn-block" type="submit"><i class="fas fa-user-plus"></i> Registrarse</button>
-                <a href="#" id="cancel_signup"><i class="fas fa-angle-left"></i> Atras</a>
-            </form>
-            <br>
-            
+            <button class="btn btn-primary btn-block" type="submit"><i class="fas fa-user-plus"></i> Registrarse</button>
+            <a href="#" id="cancel_signup"><i class="fas fa-angle-left"></i> Atras</a>
+        </form>
+        <br>
+
     </div>
     <p style="text-align:center">
         <a href="http://bit.ly/2RjWFMfunction toggleResetPswd(e){
@@ -79,10 +68,11 @@ $(()=>{
     $('#logreg-forms #cancel_reset').click(toggleResetPswd);
     $('#logreg-forms #btn-signup').click(toggleSignUp);
     $('#logreg-forms #cancel_signup').click(toggleSignUp);
-})g" target="_blank" style="color:black">By Artin</a>
+})g" target="_blank" style="color:black"></a>
     </p>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="loguin.js"></script>
 </body>
+
 </html>
